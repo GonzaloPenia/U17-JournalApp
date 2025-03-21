23e489c6f1ff3f40b381b2efebf4dd385a838a72
Gonzalo <gonzalo.penia1998@gmail.com>
2025-03-21 17:59:08 -0300

    login funcionando

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,37 +1,87 @@
-import { Grid2, TextField, Typography } from "@mui/material"
-
+import { Link as RouterLink } from "react-router-dom";
+import { Box, Typography,TextField, Grid2,Button,Link } from "@mui/material";
+import { Google } from "@mui/icons-material";
+  
 export const LoginPage = () => {
-  return (
+  
+    return (
+  
+        <Box component="main"
+            sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                minHeight: "100vh",
+                bgcolor: "primary.main",
+            }}>
+            
+                <Box component="section"    
+                    sx={{
 
-    <Grid2      
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      sx={{ minHeight:'100vh',
-            backgroundColor:'primary.main',
-            padding:4
-      }}>
+                        width: {
+                            xs: "80%",
+                            sm: "60%",
+                            md: "40%",
+                            lg: "30%",
+                        },
 
-        <Grid2 
-        className='box-shadow'
-        size={3}
-        sx={{ backgroundColor:'white', 
-              padding: 3, 
-              borderRadius: 2
-        }}>
-          
-            <Typography variant='h5' sx={{mb:1}} >Login</Typography>
-            <form action="">
-                <Grid2 container>
-                    <Grid2>
-                        <TextField label="Correo" type="email" placeholder='correo@google.com'fullWidth/>
+                        bgcolor: "white",
+                        padding: { xs: 2, sm: 3, md: 4 },
+                        borderRadius: 2,
+                    }}>
+            
+                    <Typography variant="h5" sx={{ mb: 2, textAlign: "center" }} >Login</Typography>           
+                    
+                    
+                    {/* TextField's */}
+                    <Grid2 container component="form" spacing={2}>
+
+                        <Grid2 size={{ xs: 6, md: 12 }}>
+
+                            <TextField id="email" label="Email" type="email" placeholder="email@google.com" size="small" fullWidth />
+
+                        </Grid2>
+
+                        <Grid2 size={{ xs: 6, md: 12 }}>
+
+                            <TextField id="password" label="Password" type="password" placeholder="password" size="small" fullWidth />            
+                        
+                        </Grid2>
+
                     </Grid2>
-                </Grid2>
-            </form>
-        </Grid2>
-    </Grid2>
+            
+                    
+                    
+                    {/* Buttons */}
+                    <Grid2 container spacing={2} sx={{ mt: 2 }}>
+            
+                        <Grid2 size={{ xs: 12, md: 6 }}>
 
-  )
-}
+                            <Button variant="contained" fullWidth> Login </Button>
+
+                        </Grid2>
+
+                        <Grid2 size={{ xs: 12, md: 6 }}>
+
+                            <Button variant="contained" fullWidth startIcon={<Google />} >Google</Button>
+
+                        </Grid2>
+            
+                    </Grid2>
+                    
+                    
+                    
+                    {/* New account */}
+                    <Grid2 container justifyContent="end" sx={{ mt: 2 }} > 
+            
+                        <Link component={RouterLink} color="inherit" to="/auth/register"> Create an account. </Link>
+            
+                    </Grid2>
+        
+                </Box>
+    
+        </Box>
+    
+    );
+  
+  };
