7ae4db9d01d590936266bdd9b7298559689c7a41
Gonzalo <gonzalo.penia1998@gmail.com>
2025-04-01 02:56:43 -0300

    implementacion de validaciones en el register

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,65 +1,123 @@
 import { Link as RouterLink } from "react-router-dom";
 import { Box,Typography,TextField, Grid2,Button,Link } from "@mui/material";
-import { Google } from "@mui/icons-material";
 import { AuthLayout } from "../layout/AuthLayout";
-  
+import { useForm } from "../../hooks";
+
+const formData = {
+    email:'gonzalo.penia@google.com',
+    password:'123456',
+    displayName:'Gonzalo Peña',
+}    
+const formValidations = {
+    email:[ (value) => value.includes('@'),'El correo debe tener un @'],
+    password:[ (value) => value.length >=6 ,'La contraseña debe tener al menos 6 caracteres'],
+    displayName:[ (value) => value.length >=2 ,'El nombre es obligatorio'],
+}
+
 export const RegisterPage = () => {
-  
+    
+    
+    const { formState, displayName, email, password, onInputChange, 
+            isFormValid, displayNameValid, emailValid, passwordValid
+    } = useForm(formData);
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        console.log(formState);
+        
+    }
+
+
     return (
         <AuthLayout title="Creá tu cuenta:">
         
         <Box>
             <Box>
                 {/* TextField's */}
-                <Grid2 container component="form" spacing={2}>
+                <Grid2 container component="form" onSubmit={onSubmit} spacing={2}>
 
                     <Grid2 size={12}>
 
-                        <TextField id="name" label="Nombre completo" type="text" placeholder="Juan Martinez" size="small" fullWidth />
+                        <TextField 
+                            id="name" 
+                            name="displayName" 
+                            value={displayName} 
+                            onChange={onInputChange} 
+                            label="Nombre completo" 
+                            type="text" 
+                            placeholder="Juan Martinez" 
+                            size="small" 
+                            error={!displayNameValid}
+                            helperText="El nombre es obligatorio"
+                            fullWidth />
+
 
                     </Grid2>
 
                     <Grid2 size={12}>
 
-                        <TextField id="email" label="Email" type="email" placeholder="email@google.com" size="small" fullWidth />
+                        <TextField 
+                            id="email" 
+                            name="email" 
+                            value={email} 
+                            onChange={onInputChange} 
+                            label="Email" 
+                            type="email" 
+                            placeholder="email@google.com" 
+                            size="small"
+                            error={!emailValid}
+                            helperText="Email no valido" 
+                            fullWidth />
 
                     </Grid2>
 
                     <Grid2 size={12}>
 
-                        <TextField id="password" label="Contraseña" type="password" placeholder="password" size="small" fullWidth />            
+                        <TextField 
+                            id="password" 
+                            name="password" 
+                            value={password} 
+                            onChange={onInputChange} 
+                            label="Contraseña" 
+                            type="password" 
+                            placeholder="password" 
+                            size="small" 
+                            error={!passwordValid}
+                            helperText="Contraseña no valida" 
+                            fullWidth />            
                     
                     </Grid2>
 
                     <Grid2 size={12}>
 
-                        <TextField id="confirm-password" label="Confirmar contraseña" type="password" placeholder="password" size="small" fullWidth />            
-                    
+                        <TextField 
+                            id="confirm-password" 
+                            label="Confirmar contraseña" 
+                            type="password" 
+                            placeholder="password" 
+                            size="small" 
+                            fullWidth />            
+                        
                     </Grid2>
 
 
-
-
-                </Grid2>
-        
-                
-                
                 {/* Buttons */}
-                <Grid2 container spacing={2} sx={{ mt: 2 }}>
+                <Grid2 size={12}>
         
-                    <Grid2 size={{ xs: 12, md: 6 }}>
-
-                        <Button variant="contained" fullWidth> Login </Button>
-
-                    </Grid2>
-
-                    <Grid2 size={{ xs: 12, md: 6 }}>
-
-                        <Button variant="contained" fullWidth startIcon={<Google />} >Google</Button>
-
-                    </Grid2>
-        
+                        <Button 
+                            type="submit" 
+                            variant="contained" 
+                            fullWidth
+                            > Crear cuenta 
+                        </Button>
+                    
                 </Grid2>
+
+
+                </Grid2>
+        
+                
+                
                 
                 
                 
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,8 +1,14 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 
-export const useForm = ( initialForm = {} ) => {
+export const useForm = ( initialForm = {}, formValidations = {} ) => {
   
     const [ formState, setFormState ] = useState( initialForm );
+    const [formValidation, setformValidation] = useState({ });
+
+    useEffect(() => {
+      createValidator();
+    }, [formState])
+    
 
     const onInputChange = ({ target }) => {
         const { name, value } = target;
@@ -16,10 +22,22 @@ export const useForm = ( initialForm = {} ) => {
         setFormState( initialForm );
     }
 
+    const createValidator  = () => {
+        const formChecedkValues = {};
+
+        for (const formField of Object.keys(formValidations)){
+            const [fn,errorMessage] = formsValidations[formField];
+
+            formChecedkValues[`${formField}Valid`]= fn (formState[formField]) ? null : errorMessage;
+        }
+        setformValidation(formCheckedValues);
+    }
+
     return {
         ...formState,
         formState,
         onInputChange,
         onResetForm,
+        ...formValidation,
     }
 }
